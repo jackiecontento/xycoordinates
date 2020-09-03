f739d289ebc1bd76cec62c36832c2a31bc39ee53
Jackie Contento <jmc161@duke.edu>
2020-09-02 20:59:23 -0400

    Edited the unit test to match the function names given in xycoordinates.py

--- test_xycoordinates.py
+++ test_xycoordinates.py
@@ -1,11 +1,21 @@
 import pytest
 
-@pytest.mark.parameteriza("xloc, yloc", [
-        (1,2),
-        (3,4),
+@pytest.mark.parametrize("coords, m, b", [
+        ([(1,2),(4,8)], 2, 0),
+        ([(3,6),(6,12)], 2, 0),                  
         ])
 
-def test_xycoordinates(xloc, yloc):
-	from xycoordinates import on_line
-	answer = on_line(xloc)
-	assert result == expected
+def test_define_line(coords, m, b):
+	from xycoordinates import define_line
+	result = define_line(coords)
+	assert result == (m, b)
+
+@pytest.mark.parametrize("m, b, x, y", [
+        (2, 0, 10, 20),
+        (2, 0, 5, 10),                  
+        ])
+
+def test_solve_y(m, b, x, y):
+        from xycoordinates import solve_y
+        result = solve_y(m, b, x)
+        assert result == y
--- xycoordinates.py
+++ xycoordinates.py
@@ -1,24 +1,20 @@
-
 def find_coordinates():
     print("My Program")
     coords = [(2,4), (4,8)]
-    [m,b] = define_line(coords)
+    (m,b) = define_line(coords)
     x = input("Input parameter x: ")
     y = solve_y(m, b, x)
-    print("Output parameter y: {}".format(y))
-
+    print("Output parameter y: {}".format(int(y)))
 
 def define_line(coords):
-    m = (coords[0][1]-coords[1][1])/(coords[0][0]-coords[1][0])
-    b = coords[0][1]-(m*coords[0][0])
+    m = int((coords[0][1]-coords[1][1])/(coords[0][0]-coords[1][0]))
+    b = int(coords[0][1]-(m*coords[0][0]))
+    print("Slope: {}, Offset: {}".format(m, b))
     return m, b
 
 def solve_y(m, b, x):
-    y_value = (m*int(x)) + b
+    y_value = int((m*int(x)) + b)
     return y_value
 
 if "__name__" == "__main__":
     find_coordinates()
-    
-    
-    
